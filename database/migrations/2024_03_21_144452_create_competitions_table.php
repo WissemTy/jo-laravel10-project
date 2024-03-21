c1d13a77e2bed517f03b44af76054c6ef8643489
WissemTy <wkaouss@gmail.com>
2024-03-21 16:24:28 +0100

    add all migration for database

--- database/migrations/2024_03_21_144452_create_competitions_table.php
+++ database/migrations/2024_03_21_144452_create_competitions_table.php
@@ -11,23 +11,22 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('competitions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('role');
-            $table->rememberToken();
+            $table->foreignId('sports_id')->unique();
+            $table->foreignId('places_id')->unique();
+            $table->timestamp('timeStart');
+            $table->timestamp('timeEnd');
+            $table->float('price', 2);
             $table->timestamps();
         });
     }
-
+    
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('competitions');
     }
 };
