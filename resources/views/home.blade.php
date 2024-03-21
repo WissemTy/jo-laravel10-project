d3c97af48aea42cc26753e1a1cabe38d57c8be42
WissemTy <wkaouss@gmail.com>
2024-03-21 14:46:40 +0100

    init header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    @include("templateHeader")
 </head>
 <body>
-    
+    a
 </body>
 </html>
